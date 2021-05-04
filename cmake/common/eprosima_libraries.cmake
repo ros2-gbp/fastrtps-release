a173ab0e434181a13ea52f0e84744e2061920c45
Steven! Ragnarök <steven@nuclearsandwich.com>
2021-05-04 15:48:49 -0700

    Fix patch misapplication after overlay change.
    In 0f56cddcc1043a0784abafc6e563306c51516969 I removed the patch overlay
    directory from the rolling release track since we should be able to use
    the upstream package.xml. This caused a conflict during the application
    of the patches introduced by #14 and #15. This restores those changes
    for future releases.

--- cmake/common/eprosima_libraries.cmake
+++ cmake/common/eprosima_libraries.cmake
@@ -98,7 +98,7 @@ macro(eprosima_find_package package)
             # Update submodule
             message(STATUS "Updating submodule thirdparty/${package}")
             execute_process(
-                COMMAND git submodule update --recursive --init "thirdparty/${package}"
+                COMMAND echo "Submodules were already updated by Bloom - see ros2-gbp/fastrtps-release#15"
                 WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
                 RESULT_VARIABLE EXECUTE_RESULT
                 )
@@ -225,7 +225,7 @@ macro(eprosima_find_thirdparty package thirdparty_name)
             # Update submodule
             message(STATUS "Updating submodule thirdparty/${thirdparty_name}")
             execute_process(
-                COMMAND git submodule update --recursive --init "thirdparty/${thirdparty_name}"
+                COMMAND echo "Submodules were already updated by Bloom - see ros2-gbp/fastrtps-release#15"
                 WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
                 RESULT_VARIABLE EXECUTE_RESULT
                 )
