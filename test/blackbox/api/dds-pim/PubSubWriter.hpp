233a62094432f43c3fa7c764debd2f6bac39120c
Steven! Ragnarök <steven@nuclearsandwich.com>
2021-05-04 14:01:50 -0700

    Rebase from 'debian/galactic/fastrtps'

--- test/blackbox/api/dds-pim/PubSubWriter.hpp
+++ test/blackbox/api/dds-pim/PubSubWriter.hpp
@@ -277,6 +277,11 @@ public:
             datawriter_qos_.data_sharing().off();
         }
 
+        if (use_pull_mode)
+        {
+            datawriter_qos_.properties().properties().emplace_back("fastdds.push_mode", "false");
+        }
+
         // By default, memory mode is preallocated (the most restritive)
         datawriter_qos_.endpoint().history_memory_policy = eprosima::fastrtps::rtps::PREALLOCATED_MEMORY_MODE;
 
@@ -720,6 +725,13 @@ public:
             const eprosima::fastrtps::ReliabilityQosPolicyKind kind)
     {
         datawriter_qos_.reliability().kind = kind;
+        return *this;
+    }
+
+    PubSubWriter& mem_policy(
+            const eprosima::fastrtps::rtps::MemoryManagementPolicy mem_policy)
+    {
+        datawriter_qos_.endpoint().history_memory_policy = mem_policy;
         return *this;
     }
 
