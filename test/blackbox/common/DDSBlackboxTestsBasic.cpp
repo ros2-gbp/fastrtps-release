666548b9582c3c0bbce54b4cb4e2fa05b3835ab4
Miguel Company <miguelcompany@eprosima.com>
2022-12-01 13:09:28 +0100

    Rebase from 'upstream'

--- test/blackbox/common/DDSBlackboxTestsBasic.cpp
+++ test/blackbox/common/DDSBlackboxTestsBasic.cpp
@@ -12,6 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <condition_variable>
+#include <mutex>
+#include <thread>
+
 #include <gtest/gtest.h>
 
 #include <fastdds/dds/domain/DomainParticipant.hpp>
@@ -107,6 +111,164 @@ TEST(DDSBasic, DeleteDisabledEntities)
     participant->delete_subscriber(subscriber);
     participant->delete_topic(topic);
     factory->delete_participant(participant);
+}
+
+/**
+ * This test checks a race condition when calling DomainParticipantImpl::create_instance_handle()
+ * from different threads simultaneously. This was resulting in a `free(): invalid pointer` crash
+ * when deleting publishers created this way, as there was a clash in their respective instance
+ * handles. Not only did the crash occur, but it was also reported by TSan.
+ *
+ * The test spawns 200 threads, each creating a publisher and then waiting on a command from the
+ * main thread to delete them (so all of them at deleted at the same time).
+ */
+TEST(DDSBasic, MultithreadedPublisherCreation)
+{
+    /* Get factory */
+    DomainParticipantFactory* factory = DomainParticipantFactory::get_instance();
+    ASSERT_NE(nullptr, factory);
+
+    /* Create participant */
+    DomainParticipant* participant = factory->create_participant((uint32_t)GET_PID() % 230, PARTICIPANT_QOS_DEFAULT);
+    ASSERT_NE(nullptr, participant);
+
+    /* Test synchronization variables */
+    std::mutex finish_mtx;
+    std::condition_variable finish_cv;
+    bool should_finish = false;
+
+    /* Function to create publishers, deleting them on command */
+    auto thread_run =
+            [participant, &finish_mtx, &finish_cv, &should_finish]()
+            {
+                /* Create publisher */
+                Publisher* publisher = participant->create_publisher(PUBLISHER_QOS_DEFAULT);
+                ASSERT_NE(nullptr, publisher);
+
+                {
+                    /* Wait for test completion request */
+                    std::unique_lock<std::mutex> lock(finish_mtx);
+                    finish_cv.wait(lock, [&should_finish]()
+                            {
+                                return should_finish;
+                            });
+                }
+
+                /* Delete publisher */
+                ASSERT_EQ(ReturnCode_t::RETCODE_OK, participant->delete_publisher(publisher));
+            };
+
+    {
+        /* Create threads */
+        std::vector<std::thread> threads;
+        for (size_t i = 0; i < 200; i++)
+        {
+            threads.push_back(std::thread(thread_run));
+        }
+
+        /* Command threads to delete their publishers */
+        {
+            std::lock_guard<std::mutex> guard(finish_mtx);
+            should_finish = true;
+            finish_cv.notify_all();
+        }
+
+        /* Wait for all threads to join */
+        for (std::thread& thr : threads)
+        {
+            thr.join();
+        }
+    }
+
+    /* Clean up */
+    ASSERT_EQ(ReturnCode_t::RETCODE_OK, factory->delete_participant(participant));
+}
+
+TEST(DDSBasic, MultithreadedReaderCreationDoesNotDeadlock)
+{
+    // Get factory
+    DomainParticipantFactory* factory = DomainParticipantFactory::get_instance();
+    ASSERT_NE(nullptr, factory);
+
+    // Create participant
+    DomainParticipant* participant = factory->create_participant((uint32_t)GET_PID() % 230, PARTICIPANT_QOS_DEFAULT);
+    ASSERT_NE(nullptr, participant);
+
+    // Register type
+    TypeSupport type_support;
+    type_support.reset(new FixedSizedPubSubType());
+    type_support.register_type(participant);
+    ASSERT_NE(nullptr, type_support);
+
+    // Create subscriber
+    Subscriber* subscriber = participant->create_subscriber(SUBSCRIBER_QOS_DEFAULT);
+    ASSERT_NE(nullptr, subscriber);
+
+    // Create publisher
+    Publisher* publisher = participant->create_publisher(PUBLISHER_QOS_DEFAULT);
+    ASSERT_NE(nullptr, publisher);
+
+    // Create Topic
+    Topic* topic = participant->create_topic(TEST_TOPIC_NAME, type_support.get_type_name(), TOPIC_QOS_DEFAULT);
+    ASSERT_NE(nullptr, topic);
+
+    // Set QoS
+    DataSharingQosPolicy dsp;
+    dsp.off();
+
+    DataWriterQos dw_qos;
+    DataReaderQos dr_qos;
+    dw_qos.data_sharing(dsp);
+    dr_qos.data_sharing(dsp);
+
+    // Create DataWriter
+    DataWriter* writer = publisher->create_datawriter(topic, dw_qos);
+    ASSERT_NE(nullptr, writer);
+
+    std::mutex mtx;
+    std::condition_variable cv;
+    bool should_finish = false;
+
+    auto thread_run = [subscriber, topic, &mtx, &cv, &should_finish, &dr_qos]()
+            {
+                // Create reader
+                DataReader* reader = subscriber->create_datareader(topic, dr_qos);
+                ASSERT_NE(nullptr, reader);
+
+                // Wait for test completion request
+                std::unique_lock<std::mutex> lock(mtx);
+                cv.wait(lock, [&should_finish]()
+                        {
+                            return should_finish;
+                        });
+
+                ASSERT_EQ(ReturnCode_t::RETCODE_OK, subscriber->delete_datareader(reader));
+            };
+
+    {
+        std::vector<std::thread> threads;
+        for (size_t i = 0; i < 10; ++i)
+        {
+            threads.push_back(std::thread(thread_run));
+        }
+
+        {
+            std::lock_guard<std::mutex> guard(mtx);
+            should_finish = true;
+            cv.notify_all();
+        }
+
+        for (std::thread& thr : threads)
+        {
+            thr.join();
+        }
+    }
+
+    ASSERT_EQ(ReturnCode_t::RETCODE_OK, publisher->delete_datawriter(writer));
+    ASSERT_EQ(ReturnCode_t::RETCODE_OK, participant->delete_publisher(publisher));
+    ASSERT_EQ(ReturnCode_t::RETCODE_OK, participant->delete_subscriber(subscriber));
+    ASSERT_EQ(ReturnCode_t::RETCODE_OK, participant->delete_topic(topic));
+    ASSERT_EQ(ReturnCode_t::RETCODE_OK, factory->delete_participant(participant));
 }
 
 } // namespace dds
